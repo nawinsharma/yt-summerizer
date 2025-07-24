c351caab72ee0fe3ae62906bebe117d97550f417
nawinsharma <nawinsharma60@gmail.com>
2025-07-24 17:59:22 +0530

    Add Summarize page component for displaying summaries
    - Introduced a new Summarize component that fetches and displays summary data based on the provided ID.
    - Implemented error handling to return a not found response if the ID is invalid or if no summary is found.
    - Enhanced user experience by integrating the SummaryBase component for structured summary presentation.

---
+++ src/app/summarize/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { getSummary } from "@/actions/fetchActions";
+import SummaryBase from "@/components/summary/SummaryBase";
+import { notFound } from "next/navigation";
+import React from "react";
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function Summarize({
+  params,
+}: PageProps) {
+  const resolvedParams = await params;
+  const { id } = resolvedParams;
+  
+  if (!id) {
+    return notFound();
+  }
+  
+  const summary = await getSummary(id);
+  if (!summary) {
+    return notFound();
+  }
+  
+  return (
+    <div className="fixed inset-0 bg-black z-50 overflow-y-auto">
+      <SummaryBase summary={summary} />
+    </div>
+  );
+} 
